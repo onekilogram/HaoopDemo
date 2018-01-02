b0d90682407ef4e1b99f744a69ca69a57d1ab9a0
kg <1015014076@qq.com>
2018-01-02 09:02:16 +0800

    数据生成线程

--- src/main/java/org/hit/data/gen/ExecutorGen.java
+++ src/main/java/org/hit/data/gen/ExecutorGen.java
@@ -1,34 +1,90 @@
 package org.hit.data.gen;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.hit.data.random.AbstractRandomInt;
+import org.hit.data.table.Field;
+import org.hit.data.table.Globals;
 import org.hit.data.table.TableInfo;
+import org.hit.data.utils.RandomCreateUtils;
 
 public class ExecutorGen implements Runnable {
-	
+
 	public TableInfo tableInfo;
 
 	public ExecutorGen(TableInfo tableInfo) {
 		this.tableInfo = tableInfo;
 	}
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		//根据每一个Field的属性，分配响应的Random随机生成函数
+		// 根据每一个Field的属性，分配响应的Random随机生成函数
 		int FieldLength = tableInfo.table.getFields().length;
-		AbstractRandomInt [] randomInts = new AbstractRandomInt[FieldLength];
-		//根据属性特点返回一个随机生成函数
-		for(int i=0;i<FieldLength;i++){
-			randomInts[i]=
+		Field[] fields = tableInfo.table.getFields();
+		int rowCount = (int) tableInfo.numTuples;
+		AbstractRandomInt<?>[] randomInts = new AbstractRandomInt[FieldLength];
+		// 根据属性特点返回一个随机生成函数
+		for (int i = 0; i < FieldLength; i++) {
+			randomInts[i] = RandomCreateUtils.getRandomByField(fields[i]);
 		}
-		
-		
-		
-		
-		
-		
-		
+		// new 一个以文件的
+
+		File file = new File(tableInfo.tableName + ".txt");
+
+		// if file doesnt exists, then create it
+		// if (!file.exists())
+		{
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		// true = append file
+		FileWriter fileWritter = null;
+		try {
+			fileWritter = new FileWriter(file.getName(), false);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+
+		for (int i = 0; i < 1500000; i++) {
+			//getRow(randomInts);
+			try {
+				bufferWritter.write(getRow(randomInts) + "\n");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		try {
+			bufferWritter.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
+
+	public String getRow(AbstractRandomInt<?>[] randomInts) {
+
+		StringBuilder stringBuilder = new StringBuilder();
+
+		for (AbstractRandomInt<?> abstractRandomInt : randomInts) {
+			stringBuilder.append(abstractRandomInt.nextValue() + Globals.DELIMITER);
+			abstractRandomInt.rowFinished();
+		}
+		return stringBuilder.toString();
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- src/main/java/org/hit/data/gen/GenerateData.java
+++ src/main/java/org/hit/data/gen/GenerateData.java
@@ -1,24 +1,32 @@
 package org.hit.data.gen;
 
+import java.util.Map;
+
 import org.hit.data.table.Globals;
+import org.hit.data.table.TableInfo;
 import org.hit.data.utils.confUtils;
 
 public class GenerateData {
 
-	
-	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
-		Globals.tableInfoMap= confUtils.getConfigByName("configuration.xml");
-		
-		//解析TableInfos
-		
-		
-		
-		
-		
-		
+		Globals.tableInfoMap = confUtils.getConfigByName("configuration.xml");
+
+		// 解析TableInfos
+		long startTime = System.currentTimeMillis(); // 获取开始时间
+
+		for (Map.Entry<String, TableInfo> entry : Globals.tableInfoMap.entrySet()) {
+
+			Thread thread = new Thread(new ExecutorGen(entry.getValue()));// 多线程实现多表的生成
+			thread.start();
+			thread.join();
+		}
+		// thread.join();
+		long endTime = System.currentTimeMillis(); // 获取结束时间
+
+		System.out.println("程序运行时间：" + (endTime - startTime) + "ms"); // 输出程序运行时间
+
 	}
 
 }
